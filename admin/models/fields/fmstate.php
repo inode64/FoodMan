2c029ae50c866023875c62aa703e14b3e82d81fe
Fco Javier Felix <ffelix@inode64.com>
2020-04-15 23:01:51 +0200

    Create FmState and use this new field in all forms

---
+++ admin/models/fields/fmstate.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package    FoodMan
+ *
+ * @author     Fco.Javier Félix <ffelix@inode64.com>
+ * @copyright  Copyright (C) 2020. All Rights Reserved
+ * @license    GNU General Public License version v3 or later; see LICENSE.txt
+ * @link       https://github.com/inode64/FoodMan
+ */
+
+namespace Joomla\CMS\Form\Field;
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * State field.
+ *
+ * @since  3.2
+ */
+class FMStateField extends JFormFMFieldPredefinedList
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var           string
+	 * @since   3.2
+	 */
+	protected $type = 'FMState';
+
+	/**
+	 * Available options
+	 *
+	 * @var        array
+	 * @since  3.2
+	 */
+	protected $predefinedOptions = array(
+		'1'  => 'JPUBLISHED',
+		'0'  => 'JUNPUBLISHED',
+		'2'  => 'JARCHIVED',
+		'-1' => 'JTRASHED'
+	);
+}
