350ba68020b38d45b44c1e796aea401cd69ef27b
Fco Javier Felix <ffelix@inode64.com>
2020-04-25 11:42:36 +0200

    It include support for min, max stock and open days for a product

--- admin/views/product/tmpl/edit.php
+++ admin/views/product/tmpl/edit.php
@@ -75,6 +75,9 @@ JFactory::getDocument()->addScriptDeclaration('
 
 		<?php echo JHtml::_('bootstrap.addTab', 'productTab', 'otherparams', JText::_('COM_FOODMAN_PRODUCT_LABEL_DETAILS')); ?>
 		<?php echo $this->form->renderField('expiration'); ?>
+	    <?php echo $this->form->renderField('minstock'); ?>
+	    <?php echo $this->form->renderField('maxstock'); ?>
+	    <?php echo $this->form->renderField('daysopen'); ?>
 		<?php echo JHtml::_('bootstrap.endTab'); ?>
 
 
